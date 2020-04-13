bd44bfd61206a4de66142b759274b00e129a0115
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2020-04-14 00:32:28 +0100

    Initial Unity Commit
    -Small bit of work done on UI

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public enum MenuState { StartUp,LogIn,SignUp}
+
+    public GameObject StartUpMenu;
+    public GameObject LogInMenu;
+    public GameObject SignUpMenu;
+
+    private MenuState menuState;
+
+    void Start()
+    {
+        SetMenuState(MenuState.StartUp);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetMenuState(MenuState state)
+    {
+        switch (state)
+        {
+            default:
+            case MenuState.StartUp:
+                StartUpMenu.SetActive(true);
+                LogInMenu.SetActive(false);
+                SignUpMenu.SetActive(false);
+                break;
+
+            case MenuState.LogIn:
+                StartUpMenu.SetActive(false);
+                LogInMenu.SetActive(true);
+                SignUpMenu.SetActive(false);
+                break;
+
+            case MenuState.SignUp:
+                StartUpMenu.SetActive(false);
+                LogInMenu.SetActive(false);
+                SignUpMenu.SetActive(true);
+                break;
+        }
+    }
+
+    public void SetMenuState(string state)
+    {
+        switch (state.ToLower())
+        {
+            default:
+            case "startup":
+                SetMenuState(MenuState.StartUp);
+                break;
+
+            case "login":
+                SetMenuState(MenuState.LogIn);
+                break;
+
+            case "signup":
+                SetMenuState(MenuState.SignUp);
+                break;
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
