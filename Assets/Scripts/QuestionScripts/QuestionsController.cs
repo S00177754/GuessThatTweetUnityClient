32a07dc05941e232a7852f2e575bedc5a25de612
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2020-04-17 18:27:40 +0100

    Further work on question setup

---
+++ Assets/Scripts/QuestionScripts/QuestionsController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PokemonAPIQuestionGenerator))]
+public class QuestionsController : MonoBehaviour
+{
+    public GameObject MCQQuestion;
+    public GameObject PokedexNumberQuestion;
+    public GameObject FillInTheBlankQuestion;
+
+    public Image spriteToDisplay;
+
+    public void SetSprite(string url)
+    {
+        StartCoroutine(DownloadSprite(url));
+    }
+
+    IEnumerator DownloadSprite(string url)
+    {
+        UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(url);
+        DownloadHandler handler = webRequest.downloadHandler;
+
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.isHttpError || webRequest.isNetworkError)
+        {
+            UnityEngine.Debug.Log("Error while Receiving: " + webRequest.error);
+        }
+        else
+        {
+            UnityEngine.Debug.Log("Success");
+
+            //Load Image
+            Texture2D texture2d = DownloadHandlerTexture.GetContent(webRequest);
+
+            Sprite sprite = null;
+            sprite = Sprite.Create(texture2d, new Rect(0, 0, texture2d.width, texture2d.height), Vector2.zero);
+
+            if (sprite != null)
+            {
+                spriteToDisplay.sprite = sprite;
+            }
+        }
+    }
+}
